305feaa6cc9f866838cf247ed682ef726eeefab9
moraes-caroline <moraes.caroline@hotmail.com>
2026-07-29 17:38:20 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-observability"
+    bucket = "terraform-state-observability-035885606922"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
